f7624b16c438e8b47c9cfbb2990873522d2e6f02
Prali7 <Pralishagiri98@gmail.com>
2024-12-07 10:22:15 +0545

    first commit

---
+++ custom.js
@@ -0,0 +1,4 @@
+var name ="pralisha"
+        var age= 18
+      //console.log("My name is "+ name + " age " +age)
+      console.log(`my name is ${name} age ${age}`)
---
+++ demo.js
@@ -0,0 +1,12 @@
+// username ="pralisha"
+// console.log(username)
+let name='krisha'
+let email='krisha@gmail.com'
+let roll_no=12
+let address = 'kapan'
+let phone=9815673425
+document.getElementById('a').innerHTML=name
+document.getElementById('b').innerHTML=email
+document.getElementById('c').innerHTML=roll_no
+document.getElementById('d').innerHTML=address
+document.getElementById('e').innerHTML=phone
